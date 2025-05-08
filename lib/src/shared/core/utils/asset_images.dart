d989b8a9b8b6252978eeafac5e0d8c93bcbb3ad3
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2025-05-08 10:04:19 -0400

    Assets Architecture demo

---
+++ lib/src/shared/core/utils/asset_images.dart
@@ -0,0 +1,10 @@
+class AssetImages {
+  //Logo
+  static const String brandIsotype =
+      'assets/icons/logos/venetiktok_brand_isotype.png';
+  static const String whiteImagotype =
+      'assets/icons/logos/venetiktok_white_imagotype.png';
+
+  //Empty
+  static const String emptyImage = 'assets/images/empty_image.png';
+}
