25e8f3f7c15102ddd9999a3307e4586d518db36c
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2025-05-21 00:59:23 -0400

    Add global logo image

--- lib/src/shared/core/utils/asset_images.dart
+++ lib/src/shared/core/utils/asset_images.dart
@@ -5,6 +5,8 @@ class AssetImages {
   static const String whiteImagotype =
       'assets/icons/logos/venetiktok_white_imagotype.png';
 
+  static const String mainLogo = "assets/icons/logos/fd_media_global_logo_without_background.png";
+
   //Empty
   static const String emptyImage = 'assets/images/empty_image.png';
 }
