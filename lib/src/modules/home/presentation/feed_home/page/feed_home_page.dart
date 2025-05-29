ba7b46c5ae9e38fa964866d9065afb58bb8c0f66
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2025-05-28 23:47:03 -0400

    Update dependencies home module

--- lib/src/modules/home/presentation/feed_home/page/feed_home_page.dart
+++ lib/src/modules/home/presentation/feed_home/page/feed_home_page.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_animated_icon_button/tap_fill_icon.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
 import 'package:video_player/video_player.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:venetiktok/l10n/l10n.dart';
@@ -9,8 +13,20 @@ import 'package:venetiktok/src/shared/features/widgets/text_page_header_widget.d
 import 'package:wolt_modal_sheet/wolt_modal_sheet.dart';
 import 'package:better_player_plus/better_player_plus.dart';
 
+import '../../../../../routes/shell_branches.dart';
+import '../../../../../shared/core/utils/asset_icons.dart';
+import '../../../../../shared/features/blocs/get_current_user_bloc/bloc.dart';
+import '../../../../../shared/features/presentation/premium_payments/page/premium_page.dart';
+import '../../../../../shared/features/use_cases/get_current_user.dart';
 import '../../../../../shared/features/widgets/custom_modal.dart';
+import '../../../../../shared/features/widgets/custom_snackbar.dart';
+import '../../../../../shared/features/widgets/custom_tag.dart';
+import '../../../../../variables/constants/constants.dart';
 import '../../../../../variables/values/values.dart';
+import '../../../../profile/presentation/profile_info/page/profile_info_page.dart';
+import '../../../domain/entities/entities.dart';
+import '../../../domain/use_cases/get_new_videos_use_case.dart';
+import '../bloc/bloc.dart';
 
 part '../widgets/feed_home_body.dart';
 
@@ -27,10 +43,30 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          lazy: false,
+          create: (context) => GetCurrentUserBloc(
+            getCurrentUserUseCase: GetCurrentUserUseCase(
+              userRepository: context.read(),
+              authRepository: context.read(),
+            ),
+          ),
+        ),
+        BlocProvider(
+          lazy: false,
+          create: (context) => UpdateFeedBloc(
+            getNewVideosUseCase: GetNewVideosUseCase(
+              mediaRepository: context.read(),
+            ),
+          )..add(FetchFeedVideosEvent(userId: 1)),
+        ),
+      ],
+      child: Scaffold(
         body: _HomeView(),
-      );
-
+      ),
+    );
   }
 }
 
@@ -39,6 +75,10 @@ class _HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _HomeBody();
+    return //MultiBlocListener(
+      //listeners: [],
+      //child:
+      _HomeBody();
+    //);
   }
 }
